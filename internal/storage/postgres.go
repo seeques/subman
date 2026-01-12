c984d025d74469a7d7de7d9cbdc75ddc6c9286b5
seeques <equestrianbuddy@gmail.com>
2026-01-12 19:39:09 +0300

    Add pgxpool creation logic

---
+++ internal/storage/postgres.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/seeques/test_junior/internal/config"
+)
+
+type PostgresStorage struct {
+	pool *pgxpool.Pool
+}
+
+func NewPostgresStorage(pool *pgxpool.Pool) *PostgresStorage {
+	return &PostgresStorage{pool: pool}
+}
+
+func CreatePool() (*pgxpool.Pool, error) {
+	cfg := config.LoadConfig()
+	conn, err := pgxpool.New(context.Background(), cfg.DatabaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, err
+}
