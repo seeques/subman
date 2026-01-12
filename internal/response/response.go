d14a12916a45839c4dddb1bb8f8a56ab66860f89
seeques <equestrianbuddy@gmail.com>
2026-01-12 20:55:09 +0300

    Add response functions for handling diff responses

---
+++ internal/response/response.go
@@ -0,0 +1,18 @@
+package response
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func RespondJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func RespondError(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Contetnt-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
