95343ed8b36165789e53e9abfa7329a7e504a344
seeques <equestrianbuddy@gmail.com>
2026-01-16 10:46:08 +0300

    Fix typo

--- internal/storage/subscription.go
+++ internal/storage/subscription.go
@@ -9,7 +9,7 @@ import (
 
 func (s *PostgresStorage) CreateSubscription(ctx context.Context, sub *models.Subscription) error {
 	query := `INSERT INTO subscription (service_name, price, user_id, start_date, end_date)
-	VALUES ($1, $2, $3, $4, &5)
+	VALUES ($1, $2, $3, $4, $5)
 	RETURNING id, service_name, price, user_id, start_date, end_date, created_at, updated_at`
 
 	err := s.pool.QueryRow(ctx, query, sub.ServiceName, sub.Price, sub.UserID, sub.StartDate, sub.EndDate).Scan(
