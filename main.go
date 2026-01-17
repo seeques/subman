edf7b93fb680f5ce6c7f4b085f77a4c2da6feca4
seeques <equestrianbuddy@gmail.com>
2026-01-17 10:25:41 +0300

    Run the server and implement graceful shutdown

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"os"
+	"log"
+	"log/slog"
+	"syscall"
+	"context"
+	"time"
+	"os/signal"
+	"net/http"
+	// "github.com/rs/zerolog/log"
+	"github.com/seeques/test_junior/internal/api"
+	"github.com/seeques/test_junior/internal/config"
+	"github.com/seeques/test_junior/internal/storage"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	slog.Info("starting server", "port", cfg.Port)
+
+	pool, err := storage.CreatePool()
+	if err != nil {
+		log.Fatalf("pgxpool creation failed: %v", err)
+	}
+	defer pool.Close()
+
+	storage := storage.NewPostgresStorage(pool)
+
+	s := api.NewServer(storage, cfg)
+
+	go func() {
+		if err := s.Run(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	slog.Info("received shutdown signal")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		slog.Error("server forced to shutdown", "error", err)
+	}
+
+	slog.Info("server stopped")
+}
