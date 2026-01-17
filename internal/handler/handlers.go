683abc1bba3ad0f36622a16be27e45c95805c7f8
seeques <equestrianbuddy@gmail.com>
2026-01-17 10:18:04 +0300

    Fix typos

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,9 +1,8 @@
 package handler
 
 import (
-	"context"
+	"time"
 
-	"github.com/rs/zerolog"
 	"github.com/seeques/test_junior/internal/config"
 	"github.com/seeques/test_junior/internal/storage"
 )
@@ -11,15 +10,12 @@ import (
 type Handler struct {
 	storage *storage.PostgresStorage
 	cfg     config.Config
-	logger  zerolog.Logger
-
 }
 
-func NewHandler(storage *storage.PostgresStorage, cfg config.Config, logger zerolog.Logger) *Handler {
+func NewHandler(storage *storage.PostgresStorage, cfg config.Config) *Handler {
 	return &Handler{
 		storage: storage,
 		cfg:     cfg,
-		logger:  logger.With().Str("component", "handler").Logger(),
 	}
 }
 
@@ -32,12 +28,12 @@ type SubscriptionRequest struct {
 }
 
 type SubscriptionResponse struct {
-	ID          string  `json:"id"`
-	ServiceName string  `json:"service_name"`
-	Price       int     `json:"price"`
-	UserID      string  `json:"user_id"`
-	StartDate   string  `json:"start_date"`
-	EndDate     *string `json:"end_date,omitempty"`
-	CreatedAt   string  `json:"created_at"`
-	UpdatedAt   string  `json:"updated_at"`
+	ID          int       `json:"id"`
+	ServiceName string    `json:"service_name"`
+	Price       int       `json:"price"`
+	UserID      string    `json:"user_id"`
+	StartDate   string    `json:"start_date"`
+	EndDate     *string   `json:"end_date,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
--- internal/response/response.go
+++ internal/response/response.go
@@ -12,7 +12,7 @@ func RespondJSON(w http.ResponseWriter, status int, data interface{}) {
 }
 
 func RespondError(w http.ResponseWriter, status int, message string) {
-	w.Header().Set("Contetnt-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]string{"error": message})
 }
